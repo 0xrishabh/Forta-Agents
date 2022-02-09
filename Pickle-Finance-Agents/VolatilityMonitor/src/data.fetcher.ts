4a0e05de544335ab47eb78765b7ad1d3e7593605
Lázaro Raúl <riglesiasvera@gmail.com>
2022-02-09 01:05:34 -0500

    feat: Avoid keeping all the addresses in memory

--- Pickle-Finance-Agents/VolatilityMonitor/src/data.fetcher.ts
+++ Pickle-Finance-Agents/VolatilityMonitor/src/data.fetcher.ts
@@ -2,7 +2,7 @@ import { BigNumberish, Contract, providers, utils } from "ethers";
 import LRU from "lru-cache";  
 import abi from "./abi";
 
-type ResultType = Promise<string[] | string>;
+type ResultType = Promise<string | number>;
 
 export default class DataFetcher {
   readonly registry: string;
@@ -18,7 +18,7 @@ export default class DataFetcher {
   }
 
   public async getUpkeep(block: number, id: BigNumberish): Promise<string> {
-    const key: string = `${block}-${id.toString()}`;
+    const key: string = `upkeep-${block}-${id.toString()}`;
     if(this.cache.has(key))
       return this.cache.get(key) as Promise<string>;
 
@@ -29,25 +29,44 @@ export default class DataFetcher {
     return address;
   }
 
-  public async getStrategies(block: number, keeper: string): Promise<string[]> {
-    const key: string = `${block}-${keeper}`;
+  public async getStrategiesLength(block: number, keeper: string): Promise<number> {
+    const key: string = `lenght-${block}-${keeper}`;
     if(this.cache.has(key))
-      return this.cache.get(key) as Promise<string[]>;
+      return this.cache.get(key) as Promise<number>;
+
+    const length: Promise<number> = await this.provider
+      .getStorageAt(keeper, 0, block)
+      .then((encodedLength: string) => utils.defaultAbiCoder
+        .decode(['uint256'], encodedLength)[0]
+      );
+
+    this.cache.set(key, length);
+    return length;
+  }
+
+  public async getStrategy(block: number, keeper: string, idx: number): Promise<string> {
+    const key: string = `strat-${block}-${keeper}-${idx}`;
+    if(this.cache.has(key))
+      return this.cache.get(key) as Promise<string>;
 
     const kContract: Contract = new Contract(keeper, abi.KEEPER, this.provider);
-    const encodedLength: string = await this.provider.getStorageAt(keeper, 0, block);
-    const length: number = utils.defaultAbiCoder.decode(['uint256'], encodedLength)[0];
 
-    const strategiesPromises: Promise<string>[] = [];
-    for (let i = 0; i < length; ++i){
-      strategiesPromises.push(
-        kContract
-          .strategyArray(i, { blockTag: block })
-          .then((strat: string) => strat.toLowerCase())
-      )
-    }
-    const strategies: Promise<string[]> = Promise.all(strategiesPromises);
-    this.cache.set(key, strategies);
-    return strategies;
+    const strategy: Promise<string> = kContract
+      .strategyArray(idx, { blockTag: block })
+      .then((strat: string) => strat.toLowerCase());
+    
+    this.cache.set(key, strategy);
+    return strategy;
+  }
+
+  // for testing purposes
+  public async getStrategies(block: number, keeper: string): Promise<string[]> {
+    const length: number = await this.getStrategiesLength(block, keeper);
+
+    const strategies: Promise<string>[] = [];
+    for(let i = 0; i < length; ++i)
+      strategies.push(this.getStrategy(block, keeper, i));
+    
+    return Promise.all(strategies);
   }
 };
--- Pickle-Finance-Agents/VolatilityMonitor/src/main.handler.ts
+++ Pickle-Finance-Agents/VolatilityMonitor/src/main.handler.ts
@@ -25,40 +25,36 @@ const getMainHandler = (
     const block: number = txEvent.blockNumber;
     const timestamp: number = txEvent.timestamp;
 
-    // check huge periods without performing a strategy
-    const upkeeps: string[] = await Promise.all(
-      idsList.map(id => fetcher.getUpkeep(block, id))
-    );
-    const strategies: string[][] = await Promise.all(
-      upkeeps.map(upkeep => fetcher.getStrategies(block, upkeep))
-    );
-    for(let i = 0; i < ids.size; ++i) {
-      for(let strat of strategies[i]) {
-        const last: number = mem.getLast(upkeeps[i], strat);
+    for(let id of idsList) {
+      const upkeep: string = await fetcher.getUpkeep(block, id);
+
+      // check huge periods without performing a strategy
+      const length: number = await fetcher.getStrategiesLength(block, upkeep);
+      for(let i = 0; i < length; ++i) {
+        const strategy: string = await fetcher.getStrategy(block, upkeep, i);
+        const last: number = mem.getLast(upkeep, strategy);
         if((last === -1) || (timestamp - last >= hugePeriod)) {
           findings.push(utils.notCalledFinding(
-            idsList[i],
-            upkeeps[i],
-            strat,
+            id,
+            upkeep,
+            strategy,
             timestamp - Math.max(last, 0),
             hugePeriod,
           ));
         }
       }
-    }
 
-    // Analize addition/removals in the keepers
-    upkeeps.forEach((keeper: string) => {
+      // Analize addition/removals in the keepers
       txEvent
-        .filterFunction(abi.STRATEGIES_MANAGMENT, keeper)
+        .filterFunction(abi.STRATEGIES_MANAGMENT, upkeep)
         .forEach((desc: TransactionDescription) =>
           // @ts-ignore
           mem[desc.name](
-            keeper, 
+            upkeep, 
             desc.args[0].toLowerCase(), 
             timestamp)
-          )
-    });  
+          );
+    }
 
     // Detect performUpkeep calls
     const logs: LogDescription[] = txEvent.filterLog(abi.REGISTRY, fetcher.registry);
